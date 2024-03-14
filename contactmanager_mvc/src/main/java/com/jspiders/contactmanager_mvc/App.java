d5168ab24778dabb9906b630ac7a6eee92485393
dannytayade1495 <dannytayade1495@gmail.com>
2024-03-14 23:37:31 +0530

    final commit

---
+++ contactmanager_mvc/src/main/java/com/jspiders/contactmanager_mvc/App.java
@@ -0,0 +1,5 @@
+package com.jspiders.contactmanager_mvc;
+
+public class App {
+
+}
---
+++ contactmanager_mvc/src/main/java/com/jspiders/contactmanager_mvc/entity/Contact.java
@@ -0,0 +1,35 @@
+package com.jspiders.contactmanager_mvc.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contact_details")
+public class Contact {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	private int id;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	@Column(name = "mobile_number")
+	private long mobileNumber;
+	private String email;
+	private String company;
+	@Column(name = "contact_group")
+	private String group;
+	
+}
